ca90c9880ddf2dbcf68cd320e1331e719999a01d
arcanegolem <dopeflipper@gmail.com>
2023-11-05 03:21:59 +0300

    Смена версии Kotlin 1.8.10 -> 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
       compose = true
    }
    composeOptions {
-      kotlinCompilerExtensionVersion = "1.4.3"
+      kotlinCompilerExtensionVersion = "1.5.2"
    }
    packaging {
       resources {
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-   implementation("androidx.core:core-ktx:1.9.0")
+   implementation("androidx.core:core-ktx:1.12.0")
    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
    implementation("androidx.activity:activity-compose:1.8.0")
    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -66,4 +66,7 @@ dependencies {
    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
    debugImplementation("androidx.compose.ui:ui-tooling")
    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+   // Play Services
+   implementation("com.google.android.gms:play-services-location:21.0.1")
 }
