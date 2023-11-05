193cdc3f1b1af4a470ae84355d59981ae70e7c9f
arcanegolem <dopeflipper@gmail.com>
2023-11-06 01:54:23 +0300

    Константы в отдельный файл

---
+++ app/src/main/java/ru/spektrit/androidsensorsdemo/util/Constants.kt
@@ -0,0 +1,10 @@
+package ru.spektrit.androidsensorsdemo.util
+
+// Константа для обозначения 500мс в мкс
+const val MILLIS_500 = 500000
+// Константа получения локации пользователя (мс)
+const val LOCATION_RETRIEVAL_INTERVAL = 500L
+
+const val RESOLUTION_REQUEST_CODE = 0x1
+
+const val SAVING_INTERVAL_MILLIS = 500L
