e05302b515204e372fc9fdeff233dd96ac92f834
arcanegolem <dopeflipper@gmail.com>
2023-11-06 18:20:16 +0300

    Изменения функции сохранения данных

--- app/src/main/java/ru/spektrit/androidsensorsdemo/SensorViewModel.kt
+++ app/src/main/java/ru/spektrit/androidsensorsdemo/SensorViewModel.kt
@@ -8,8 +8,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import ru.spektrit.androidsensorsdemo.database.dao.SensorValuesDao
-import ru.spektrit.androidsensorsdemo.database.entities.AccelerometerData
-import ru.spektrit.androidsensorsdemo.database.entities.OrientationData
+import ru.spektrit.androidsensorsdemo.database.entities.SensorsData
 import ru.spektrit.androidsensorsdemo.util.SAVING_INTERVAL_MILLIS
 import ru.spektrit.androidsensorsdemo.util.SensorType
 import java.util.Calendar
@@ -17,7 +16,7 @@ import java.util.Calendar
 /**
  * Класс отвечающий за бизнес-логику
  */
-class SensorViewModel() : ViewModel() {
+class SensorViewModel : ViewModel() {
    private var savingJob : Job? = null
    private var dao : SensorValuesDao? = null
 
@@ -26,14 +25,14 @@ class SensorViewModel() : ViewModel() {
     *
     * @param dao [SensorValuesDao] объект
     */
-   fun pinDao(dao: SensorValuesDao) { this.dao = dao }
+   fun pinDao( dao: SensorValuesDao ) { this.dao = dao }
 
    /**
     * Метод старта сохранения данных в БД
     *
     * @param sensorFlows [Map] с ключами [SensorType] и значениями в виде [MutableStateFlow] данных с сенсоров
     */
-   fun startSavingSensorsData(sensorFlows : Map<SensorType, MutableStateFlow<FloatArray>>) {
+   fun startSavingSensorsData( sensorFlows : Map<SensorType, MutableStateFlow<FloatArray>> ) {
       savingJob = viewModelScope.launch {
          while (true) {
             val calendar = Calendar.getInstance()
@@ -42,7 +41,20 @@ class SensorViewModel() : ViewModel() {
                   "${calendar.get(Calendar.MINUTE)}:${calendar.get(Calendar.SECOND)}:" +
                   "${calendar.get(Calendar.MILLISECOND)}"
             Log.i("SensorsDataSaving", "Data saving started! $currentDateAsString")
-            saveData(sensorFlows, currentDateAsString)
+
+            val sensorsData = SensorsData(
+               time = currentDateAsString,
+               accelerometer = sensorFlows[SensorType.ACCELEROMETER]!!.value.joinToString(" "),
+               orientation = sensorFlows[SensorType.ORIENTATION]!!.value.joinToString(" "),
+               gyroscope = sensorFlows[SensorType.GYROSCOPE]!!.value.joinToString(" "),
+               magneticField = sensorFlows[SensorType.MAGNETIC_FIELD]!!.value.joinToString(" "),
+               gravity = sensorFlows[SensorType.GRAVITY]!!.value.joinToString(" "),
+               geomagneticRotation = sensorFlows[SensorType.GEOMAGNETIC_ROTATION]!!.value.joinToString(" "),
+               rotationVector = sensorFlows[SensorType.ROTATION_VECTOR]!!.value.joinToString(" "),
+               computedOrientation = sensorFlows[SensorType.COMPUTED_ORIENTATION]!!.value.joinToString(" "),
+            )
+
+            saveData(sensorsData)
             delay(SAVING_INTERVAL_MILLIS)
          }
       }
@@ -59,27 +71,7 @@ class SensorViewModel() : ViewModel() {
    /**
     * Асинхронный метод непосредственного сохранения данных в локальную БД
     */
-   private suspend fun saveData(
-      sensorFlows : Map<SensorType, MutableStateFlow<FloatArray>>,
-      currentDate : String
-   ){
-      val accelerometerData = sensorFlows[SensorType.ACCELEROMETER]!!.value
-      dao?.recordData(
-         AccelerometerData(
-            time = currentDate,
-            x = accelerometerData[0],
-            y = accelerometerData[1],
-            z = accelerometerData[2]
-         )
-      )
-      val orientationData = sensorFlows[SensorType.ORIENTATION]!!.value
-      dao?.recordData(
-         OrientationData(
-            time = currentDate,
-            x = orientationData[0],
-            y = orientationData[1],
-            z = orientationData[2]
-         )
-      )
+   private suspend fun saveData( sensorsData : SensorsData ) {
+      dao?.recordSensorsData(sensorsData)
    }
 }
